427ee1e781c39b3fe08cdb6e6b82cf1938fcadd2
Guillermo <gaguilar128@gmail.com>
2022-02-05 00:17:40 -0600

    mbasic cpp practice

--- index.cpp
+++ index.cpp
@@ -1,9 +1,92 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << "Tim" << endl;
-    cout << "Hello World!" << "Joe" << endl;
-    return 0;
-}
+// Entry point into program
+// int main() {
+//     cout << "Hello World!" << "Tim" << endl;
+//     return 0;
+// }
+
+// Variable declaration
+// int main() {
+//     int x = 456;
+//     string max = "max is my name";
+//     cout << max << endl;
+// }
+
+// Constant Variables
+// int main() {
+//     const float gravity = -9.8;
+//     cout << gravity << endl;
+// }
+
+// cin failure and clear function
+// int main() {
+//     int n, x;
+//     cin >> n;
+//     cout << n << endl;
+//     cin.clear();
+//     cin.ignore(1000, '\n');
+//     cin >> x;
+//     cout << x << endl;
+// }
+
+// Simple calculator
+// int main() {
+//     int num1, num2;
+//     cout << "Enter a Number: ";
+//     cin >> num1;
+//     cin.clear();
+//     cin.ignore(1000, '\n');
+//     cout << "Enter a Second Number: ";
+//     cin >> num2;
+//     cin.clear();
+//     cin.ignore(1000, '\n');
+//     int sum = num1 + num2;
+//     cout << "The sum is: " << sum << endl;
+// }
+
+// Return a string
+// int main() {
+//     string s;
+//     cin >> s;
+//     cout << s << endl;
+// }
+
+// Math operators
+// int main() {
+//     float x = 8;
+//     float y = 9;
+//     float mult = x*y;
+//     cout << mult << endl;
+// }
+
+// Comparison operators
+// int main() {
+//     int num1, num2;
+//     cout << "Type a number: ";
+//     cin >> num1;
+//     cout << "Type a number: ";
+//     cin >> num2;
+//     cout << "These two nubmers are the same: " << (num1==num2) << endl;
+// }
+
+
+// Logical operators
+// int main() {
+//     bool x = !(1 < 2);
+//     cout << x << endl;
+// }
+
+// If, ELSE, ELSE IF
+// int main() {
+//     int x = 8;
+//     if (x > 10) {
+//         cout << "More then 8";
+//     } else {
+//         cout << "you trash!";
+//     }
+// }
+
